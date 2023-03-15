ff6838bce7154df882f0999bdc9a0869bc556595
Guilherme Gonçalves <guilhermepgx20@gmail.com>
2023-03-14 21:17:48 -0300

    card project
    Iniciando commit no projeto card

---
+++ info/main.js
@@ -0,0 +1,5 @@
+const [name, linkedin, github] = window.location.search.replace(/\?data=/, '').split('=')
+
+document.getElementById('name').innerText = name
+document.getElementById('linkedin').href = linkedin
+document.getElementById('github').href = github
---
+++ qrcode/main.js
@@ -0,0 +1,25 @@
+let name = document.getElementById('name')
+let linkedin = document.getElementById('linkedin')
+let github = document.getElementById('github')
+
+let qrcodeimage = document.getElementById('qrcode')
+
+function generateImage() {
+  qrcodeimage.innerHTML = ''
+
+  const text = `${window.location.protocol}://${window.location.host}/info?data=${name.value}=${linkedin.value}=${github.value}`
+  
+  new QRCode(qrcodeimage, {
+    text,
+    width: 256,
+    height: 256,
+    colorDark: '#000000',
+    colorLight: '#ffffff',
+    correctLevel : QRCode.CorrectLevel.H,
+  })
+
+}
+
+function clean(){
+  qrcodeimage.innerHTML = "";
+}
